cdf0b98401bc05bf4baf2bc9c3ff18f2b86227cb
琴卓 <1043665802@qq.com>
2021-06-01 15:43:03 +0800

    allDirty

--- Runtime/Component/QTweenUIColor.cs
+++ Runtime/Component/QTweenUIColor.cs
@@ -19,7 +19,7 @@ namespace QTool.Tween.Component
             get => ui.color;
             set {
                 ui.color = onlyAlpha ? new Color(ui.color.r, ui.color.g, ui.color.b, value.a) : value;
-                ui.SetVerticesDirty();
+                ui.SetAllDirty();
             }
         }
         public bool onlyAlpha = false;
