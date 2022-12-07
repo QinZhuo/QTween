05fb54268db59c7c47e1dedf2efaef5ad77cd296
琴卓 <1043665802@qq.com>
2022-12-07 17:39:29 +0800

    set

--- Runtime/Qtween/QTween.cs
+++ Runtime/Qtween/QTween.cs
@@ -19,7 +19,7 @@ namespace QTool.Tween
 		public bool IgnoreTimeScale { set;private get; } = true;
 		public bool AutoDestory { set;private get; } = true;
 		public Func<float, float> TweenCurve { get; set; } = QCurve.Linear;
-		public UnityEngine.Object Target { set;internal get; }
+		public UnityEngine.Object Target { internal set; get; }
 		#region 更改数值
 		public QTween SetCurve(EaseCurve ease)
 		{
