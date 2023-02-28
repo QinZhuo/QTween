1e55e79e0e0dd647d268ead235c10b4960035482
琴卓 <1043665802@qq.com>
2023-02-28 21:36:55 +0800

    floatEvent

---
+++ Runtime/Component/QTweenFloatEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+namespace QTool.Tween.Component
+{
+    public class QTweenFloatEvent: QTweenBehavior<float>
+    {
+        public override float CurValue
+        {
+            get => curValue; set { curValue = value; FloatEvent.Invoke(value); }
+        }
+		[QName("当前值")]
+		public float curValue =0;
+		public FloatEvent FloatEvent=new FloatEvent();
+    }
+}
