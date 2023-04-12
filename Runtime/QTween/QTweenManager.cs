56240de53778b0a4b52362a05f0b05f8e718365e
琴卓 <1043665802@qq.com>
2023-04-12 21:33:09 +0800

    适配Lerp

--- Runtime/QTween/QTweenManager.cs
+++ Runtime/QTween/QTweenManager.cs
@@ -14,13 +14,13 @@ namespace QTool.Tween
 #endif
 		static void InitValueLerp()
 		{
-			QTween<string>.ValueLerp = QLerp.LerpTo;
-			QTween<float>.ValueLerp = QLerp.LerpTo; 
-			QTween<double>.ValueLerp = QLerp.LerpTo;
-			QTween<Vector2>.ValueLerp = QLerp.LerpTo;
-			QTween<Vector3>.ValueLerp = QLerp.LerpTo;
-			QTween<Quaternion>.ValueLerp = QLerp.LerpTo;
-			QTween<Color>.ValueLerp = QLerp.LerpTo;
+			QTween<string>.ValueLerp = QLerp.Lerp;
+			QTween<float>.ValueLerp = QLerp.Lerp; 
+			QTween<double>.ValueLerp = QLerp.Lerp;
+			QTween<Vector2>.ValueLerp = QLerp.Lerp;
+			QTween<Vector3>.ValueLerp = QLerp.Lerp;
+			QTween<Quaternion>.ValueLerp = QLerp.Lerp;
+			QTween<Color>.ValueLerp = QLerp.Lerp;
 		}
 	
 
@@ -30,27 +30,27 @@ namespace QTool.Tween
         }
         public static QTween<string> Tween(Func<string> Get, Action<string> Set, string end, float duration)
         {
-            return GetTween(Get, Set, end, duration,QLerp.LerpTo);
+            return GetTween(Get, Set, end, duration,QLerp.Lerp);
         }
         public static QTween<float> Tween(Func<float> Get, Action<float> Set, float end, float duration)
         {
-           return GetTween(Get, Set, end, duration, QLerp.LerpTo);
+           return GetTween(Get, Set, end, duration, QLerp.Lerp);
         }
         public static QTween<Vector2> Tween(Func<Vector2> Get, Action<Vector2> Set, Vector2 end, float duration)
         {
-            return GetTween(Get, Set, end, duration, QLerp.LerpTo);
+            return GetTween(Get, Set, end, duration, QLerp.Lerp);
         }
         public static QTween<Vector3> Tween(Func<Vector3> Get, Action<Vector3> Set, Vector3 end, float duration)
         {
-            return GetTween(Get, Set, end, duration, QLerp.LerpTo);
+            return GetTween(Get, Set, end, duration, QLerp.Lerp);
         }
         public static QTween<Quaternion> Tween(Func<Quaternion> Get, Action<Quaternion> Set, Quaternion end, float duration)
         {
-            return GetTween(Get, Set, end, duration, QLerp.LerpTo);
+            return GetTween(Get, Set, end, duration, QLerp.Lerp);
         }
         public static QTween<Color> Tween(Func<Color> Get, Action<Color> Set, Color end, float duration)
         {
-            return GetTween(Get, Set, end, duration, QLerp.LerpTo);
+            return GetTween(Get, Set, end, duration, QLerp.Lerp);
         }
         private static QTween<T> GetTween<T>(Func<T> Get, Action<T> Set, T end, float duration,Func<T, T, float, T> ValueLerp)
         {
