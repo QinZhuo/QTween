e65aad97f2ee02691e1f5556f79d6a6599718ace
琴卓 <1043665802@qq.com>
2024-06-05 23:47:56 +0800

    修改 Runtime/Component/QTweenLoop.cs

--- Runtime/Component/QTweenLoop.cs
+++ Runtime/Component/QTweenLoop.cs
@@ -2,42 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-namespace QTool.Tween
-{
-    public class QTweenLoop : QTweenComponent
-    {
-        public QTweenComponent qTween;
+namespace QTool.Tween {
+	public class QTweenLoop : QTweenComponent {
+		public QTweenComponent qTween;
 		public override QTween Anim => qTween.Anim;
-		protected override QTween GetTween()
-		{
+		public bool repeat = false;
+		protected override QTween GetTween() {
 			return null;
 		}
-		private void Reset()
-        {
-			 qTween = GetComponent<QTweenComponent>();
-        }
+		private void Reset() {
+			qTween = GetComponent<QTweenComponent>();
+		}
 		string loopKey = QTool.GetGuid();
-		public override async Task PlayAsync(bool show)
-		{
-			if (show)
-			{
+		public override async Task PlayAsync(bool show) {
+			if (show) {
 				var flag = loopKey;
-				while (loopKey == flag && this != null&&Anim!=null)
-				{
+				while (loopKey == flag && this != null && Anim != null) {
 					Anim.SetPlayer(this).Play(true);
 					await Anim.WaitOverAsync();
-					Anim.SetPlayer(this).Play(false);
-					await Anim.WaitOverAsync();
+					if (repeat) {
+						Anim.Play(false);
+						Anim.Complete();
+					}
+					else {
+						Anim.SetPlayer(this).Play(false);
+						await Anim.WaitOverAsync();
+					}
+					
 				}
 			}
-			else
-			{
+			else {
 				loopKey = QTool.GetGuid();
 				Anim.SetPlayer(this).Play(false);
 			}
 		}
-		protected override void OnDestroy()
-		{
+		protected override void OnDestroy() {
 			loopKey = QTool.GetGuid();
 			base.OnDestroy();
 		}
